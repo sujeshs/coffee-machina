d5ec1d4d95530bc40047f5497bf52ca3da208557
sujeshs <santosterock@gmail.com>
2021-12-27 12:52:03 -0800

    feat : coffee machine blueprint

---
+++ src/main/java/com/sujeshs/coffeemachina/CoffeemachinaApplication.java
@@ -0,0 +1,13 @@
+package com.sujeshs.coffeemachina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeemachinaApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(CoffeemachinaApplication.class, args);
+	}
+
+}
